d9c99aed3c94afab1d9915d10b77fb45c2d53b72
Eric <ericpan122@gmail.com>
2023-10-14 00:25:48 -0400

    added the test code for the 8 methods

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,22 +1,23 @@
 public class LabRunner {
     public static void main(String[] args) {
         CustomStringMethods methods = new CustomStringMethods();
-        String str = methods.yellOrWhisper("Hello James!");
+        boolean bool = methods.longerThan("blue", 3);
+        System.out.println(bool);
+        String str = methods.funnyString("aqua", 1);
         System.out.println(str);
-        str = methods.yellOrWhisper("hello James!");
+        str = methods.halvesReversed("green");
         System.out.println(str);
-        str = methods.yellOrWhisper("aBCDEFGHIJK");
+        str = methods.pigLatin("gold");
         System.out.println(str);
-        str = methods.yellOrWhisper("Abcdefghijk");
+        str = methods.removeCharacter("black", 0);
         System.out.println(str);
-        str = methods.yellOrWhisper("B");
+        str = methods.insertAt("red", "er!", "d");
         System.out.println(str);
-        str = methods.yellOrWhisper("b");
+        str = methods.endUp("brown", 3);
         System.out.println(str);
-        str = methods.yellOrWhisper("IT'S SUNNY!");
+        str = methods.yellOrWhisper("Rainbow!");
         System.out.println(str);
-        str = methods.yellOrWhisper("it's rainy");
-        System.out.println(str);
+
 
     }
 }
